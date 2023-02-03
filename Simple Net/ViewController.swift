8da9192ccd103438f5f462bda94d372ccc78971c
Matvei Bykadorov <2kydepsta2@gmail.com>
2023-02-03 23:28:49 +0300

    Initial Commit

---
+++ Simple Net/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Net
+//
+//  Created by Matvei Bykadorov on 03.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
