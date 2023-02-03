fb7c43b161be61dbb0b85ea1c73909294b3c7cdf
Matvei Bykadorov <2kydepsta2@gmail.com>
2023-02-04 01:42:42 +0300

    download image

--- Simple Net/ViewController.swift
+++ Simple Net/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var getImageButton: UIButton!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
     }
 
 
+    @IBAction func getImagePressed(_ sender: UIButton) {
+        
+        label.isHidden = true
+        getImageButton.isEnabled = false
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else {return}
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { (data, response, error) in
+            guard let data = data, let image = UIImage(data: data) else {return}
+            DispatchQueue.main.async {
+                self.activityIndicator.stopAnimating()
+                self.imageView.image = image
+            }
+        }.resume()
+    }
 }
 
